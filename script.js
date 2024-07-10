f856f4e29bb3c2fa2c6918860fab2d4f88e154e3
Renanjuniior6 <renanjrr679@gmail.com>
2024-07-10 16:02:50 -0300

    toast de notificação

--- script.js
+++ script.js
@@ -8,11 +8,29 @@ btnGenerator.addEventListener('click', () => {
     // Configuração do arquivo final PDF
     const options = {
         margin: [0.5, 1, 0, 0],
-        filename: 'arquivo.pdf',
+        filename: 'cronograma.pdf',
         html2canvas: {scale: 2},
         jsPDF: { unit: 'mm', format: 'a6', orientation: 'portrait'}
     }  
 
     // Gerar e baixar o PDF
     html2pdf().set(options).from(content).save()
+
+    setTimeout(() => {
+        Toastify({
+            text: "Salvo!",
+            duration: 2000,
+            destination: "https://github.com/apvarun/toastify-js",
+            newWindow: true,
+            close: true,
+            gravity: "top", // `top` or `bottom`
+            position: "center", // `left`, `center` or `right`
+            stopOnFocus: true, // Prevents dismissing of toast on hover
+            style: {
+              background: "linear-gradient(to right, #00b09b, #96c93d)",
+            },
+          }).showToast();
+    }, 1000);
+
+    
 })
